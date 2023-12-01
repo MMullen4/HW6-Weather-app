8999240251ed7606eac1cec74bc50bae2ca62f7b
Matt Mullen <mhmullen@icloud.com>
2023-12-01 10:06:42 -0800

    debugging

--- assets/script.js
+++ assets/script.js
@@ -27,9 +27,11 @@ function GetWeather(city) {
 }
 function GetForcast(lat, lon) {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+    console.log(queryURL)
     fetch(queryURL)
         .then(function (response) {
             //    console.log(response)
+            
             return response.json()
         }).then(function (data) {
             console.log(data) // apply forcasted weather
